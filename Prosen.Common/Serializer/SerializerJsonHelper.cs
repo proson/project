53041da8c17f2b1f6bc2a5fcea1176a6dfa7cf96
刘稳 <lw3039@17u.cn>
2014-04-30 17:57:01 +0800

    项目更新

---
+++ Prosen.Common/Serializer/SerializerJsonHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Prosen.Common.Serializer
+{
+    /// <summary>
+    /// Json序列化帮助类
+    /// </summary>
+    public class SerializerJsonHelper
+    {
+        // 从一个对象信息生成Json串
+        public static string ObjectToJson(object obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, obj);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+        // 从一个Json串生成对象信息
+        public static object JsonToObject(string jsonString, object obj)
+        {
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            using (MemoryStream mStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                return serializer.ReadObject(mStream);
+            }
+
+        }
+    }
+}
